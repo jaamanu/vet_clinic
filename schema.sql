0a1c9084ca7f42ba5499eb72ec88d2de4874082c
JAMES A MANU <98466955+jaamanu@users.noreply.github.com>
2022-12-13 02:49:19 +0200

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,9 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+CREATE TABLE animal (
+id INT PRIMARY KEY NOT NULL,
+name VARCHAR(50),
+date_of_birth date,
+escape_attempts INT,
+neutered bool,
+weight_kg decimal );
