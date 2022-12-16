378e7ca92027ce636bf7a5363f7e2206a6f4c548
user <user@users-MacBook-Pro.local>
2022-12-17 01:19:53 +0200

    Update dabase

--- data.sql
+++ data.sql
@@ -36,3 +36,51 @@ VALUES (10, 'Blossom', 'Jun 13, 1998', 3, true, 17);
 
 INSERT INTO animal (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES (11, 'Dito', 'May 14, 2022', 1, true, 22);
+
+INSERT INTO owners (full_name, age)
+VALUES ('Sam Smith', 34);
+
+INSERT INTO owners (full_name, age)
+VALUES ('Jennifer Orwell', 19);
+
+INSERT INTO owners (full_name, age)
+VALUES ('Bob', 45);
+
+INSERT INTO owners (full_name, age)
+VALUES ('Melody Pond', 77);
+
+INSERT INTO owners (full_name, age)
+VALUES ('Dean Winchester', 14);
+
+INSERT INTO owners (full_name, age)
+VALUES ('Jodie Whittaker', 38);
+
+INSERT INTO species (name)
+VALUES ('Pokemon');
+
+iNSERT INTO species (name)
+VALUES ('Digimon');
+
+UPDATE animal SET species_id = 1 WHERE species_id IS NULL;
+
+UPDATE animal SET species_id = 2 WHERE name LIKE '%mon';
+
+UPDATE animal
+  SET owners_id = (SELECT id from owners WHERE full_name = 'Sam Smith')
+  WHERE name = 'Agumon';
+
+  UPDATE animal
+  SET owners_id = (SELECT id from owners WHERE full_name = 'Jennifer Orwell')
+  WHERE name IN ('Gabumon', 'Pikachu');
+
+UPDATE animal
+  SET owners_id = (SELECT id from owners WHERE full_name = 'Bob')
+  WHERE name IN ('Devimon', 'Plantmon');
+
+UPDATE animal
+  SET owners_id = (SELECT id from owners WHERE full_name = 'Melody Pond')
+  WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+
+UPDATE animal
+  SET owners_id = (SELECT id from owners WHERE full_name = 'Dean Winchester')
+  WHERE name IN ('Angemon', 'Boarmon');
