e4569fe1d62d45f8e977e4c73dd2b610eac74ba7
user <user@users-MacBook-Pro.local>
2022-12-17 08:49:16 +0200

    create jointable and modify table dtatype

--- schema.sql
+++ schema.sql
@@ -22,25 +22,55 @@ CREATE TABLE species(
   name VARCHAR(100)
 );
 
-ALTER TABLE animals
+ALTER TABLE animal
   DROP COLUMN id;
 
-ALTER TABLE animals
+ALTER TABLE animal
   ADD COLUMN id INT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY;
 
-ALTER TABLE animals
+ALTER TABLE animal
   DROP COLUMN species;
 
-ALTER TABLE animals
+ALTER TABLE animal
   ADD COLUMN species_id INT,
   ADD COLUMN owners_id INT;
 
-ALTER TABLE animals
+ALTER TABLE animal
   ADD CONSTRAINT fk_owner
   FOREIGN KEY (owner_id)
   REFERENCES owners(id);
 
-ALTER TABLE animals
+ALTER TABLE animal
   ADD CONSTRAINT fk_species
   FOREIGN KEY (species_id)
   REFERENCES species(id);
+
+CREATE TABLE vets (id INT NOT NULL GENERATED ALWAYS as IDENTITY PRIMARY KEY,
+name VARCHAR(150),
+age INT,
+date_of_graduation DATE );
+
+CREATE TABLE specializations (
+ vet_id INT,
+ species_id INT,
+ PRIMARY KEY (vet_id, species_id),
+ CONSTRAINT fk_vets
+     FOREIGN KEY (vet_id)
+         REFERENCES vets(id),
+ CONSTRAINT fk_species
+     FOREIGN KEY (species_id)
+         REFERENCES species(id)
+ );
+
+CREATE TABLE visits(
+    vet_id INT,
+    animal_id INT,
+    date_of_visit DATE,
+    PRIMARY KEY (vet_id, animal_id),
+    CONSTRAINT fk_vets
+        FOREIGN KEY (vet_id)
+            REFERENCES vets(id),
+    CONSTRAINT fk_animal
+        FOREIGN KEY (animal_id)
+            REFERENCES animal(id)
+);
